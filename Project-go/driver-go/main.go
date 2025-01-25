fca52fac04582e45c9d0ed2ff48c4b00a4f2fd4e
Atle <aabystro@stud.ntnu.no>
2025-01-25 11:18:49 +0100

    fix main

--- Project-go/driver-go/main.go
+++ Project-go/driver-go/main.go
@@ -10,79 +10,68 @@ var d elevio.MotorDirection
 var currentFloor int
 
 var drv_buttons = make(chan elevio.ButtonEvent)
-var drv_floors  = make(chan int)
-var drv_obstr   = make(chan bool)
-var drv_stop    = make(chan bool) 
+var drv_floors = make(chan int)
+var drv_obstr = make(chan bool)
+var drv_stop = make(chan bool)
 
 var b = make([]elevio.ButtonEvent, 10)
 
-func init_elevator(){
-    go elevio.PollFloorSensor(drv_floors)
-    d = elevio.MD_Up
-    elevio.SetMotorDirection(d)
+func init_elevator() {
+	go elevio.PollFloorSensor(drv_floors)
+	d = elevio.MD_Up
+	elevio.SetMotorDirection(d)
 
-    floor = <- drv_floors
-    d = elevio.MD_Stop
-    elevio.SetMotorDirection(d)
+	currentFloor = <-drv_floors
+	d = elevio.MD_Stop
+	elevio.SetMotorDirection(d)
 
 }
 
+func main() {
 
-func main(){
+	numFloors := 4
+
+	init_elevator()
+
+	elevio.Init("localhost:15657", numFloors)
+
+	//var d elevio.MotorDirection = elevio.MD_Up
+	//elevio.SetMotorDirection(d)
+
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
 
 	for {
 		select {
 		case a := <-drv_buttons:
-			fmt.Printf("Button pressed: %+v\n", a)
-			elevio.SetButtonLamp(a.Button, a.Floor, true)
+			fmt.Printf("%+v\n", a)
+			elevio.AddToQueue(a.Button, a.Floor, b)
+			elevio.LightButtons(b, numFloors)
 
-    init_elevator()
+		case a := <-drv_floors:
+			fmt.Printf("%+v\n", a)
+			currentFloor = a
+			elevio.RemoveFromQueue(currentFloor, d, b)
+			d = elevator_motion.SetDirection(currentFloor, b[0].Floor, d)
+			elevio.SetMotorDirection(d)
 
-    elevio.Init("localhost:15657", numFloors)
-    
-    //var d elevio.MotorDirection = elevio.MD_Up
-    //elevio.SetMotorDirection(d)
-    
-    go elevio.PollButtons(drv_buttons)
-    go elevio.PollFloorSensor(drv_floors)
-    go elevio.PollObstructionSwitch(drv_obstr)
-    go elevio.PollStopButton(drv_stop)
-    
-    
-    for {
-        select {
-        case a := <- drv_buttons:
-            fmt.Printf("%+v\n", a)
-            elevio.AddToQueue(a.Button, a.Floor, b)
-            elevio.LightButtons(b, numFloors)
-            
-        case a := <- drv_floors:
-            fmt.Printf("%+v\n", a)
-            if a == numFloors-1 {
-                d = elevio.MD_Down
-            } else if a == 0 {
-                d = elevio.MD_Up
-            }
-            elevio.SetMotorDirection(d)
-            
-            
-        case a := <- drv_obstr:
-            fmt.Printf("%+v\n", a)
-            if a {
-                elevio.SetMotorDirection(elevio.MD_Stop)
-            } else {
-                elevio.SetMotorDirection(d)
-            }
-            
-        case a := <- drv_stop:
-            fmt.Printf("%+v\n", a)
-            for f := 0; f < numFloors; f++ {
-                for b := elevio.ButtonType(0); b < 3; b++ {
-                    elevio.SetButtonLamp(b, f, false)
-                }
-            }
-        }
-    }    
+		case a := <-drv_obstr:
+			fmt.Printf("%+v\n", a)
+			if a {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			} else {
+				elevio.SetMotorDirection(d)
+			}
+
+		case a := <-drv_stop:
+			fmt.Printf("%+v\n", a)
+			for f := 0; f < numFloors; f++ {
+				for b := elevio.ButtonType(0); b < 3; b++ {
+					elevio.SetButtonLamp(b, f, false)
+				}
+			}
+		}
+	}
 }
-
-

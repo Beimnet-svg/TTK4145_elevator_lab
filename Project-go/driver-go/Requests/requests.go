0c706b3096148a0258c15289b5e862ba2306ce25
philliab13 <philliab@stud.ntnu.no>
2025-02-10 18:18:49 +0100

    ✨ feat(Requests): Request module done
    Pretty sure the request module should be good, tested it as good as possible with simple cases and it worked

--- Project-go/driver-go/Requests/requests.go
+++ Project-go/driver-go/Requests/requests.go
@@ -55,14 +55,64 @@ func requestsBelow(e elevio.Elevator) bool {
 	return false
 }
 
-func RequestChooseDir() {
+func RequestChooseDir(e elevio.Elevator, b elevio.ButtonType) (elevio.MotorDirection, elevio.ElevatorBehaviour) {
+	switch e.Direction {
+		case elevio.MD_Up:
+			if requestsAbove(e) {
+				return elevio.MD_Up, elevio.EB_Moving
+			} else if hasRequestAtFloor(e,b) {
+				return elevio.MD_Down, elevio.EB_DoorOpen
+			} else if requestsBelow(e) {
+				return elevio.MD_Down, elevio.EB_Moving
+			}
+			return elevio.MD_Stop, elevio.EB_Idle
+		case elevio.MD_Down:
+			if requestsBelow(e) {
+				return elevio.MD_Down, elevio.EB_Moving
+			} else if hasRequestAtFloor(e,b) {
+				return elevio.MD_Up, elevio.EB_DoorOpen
+			} else if requestsAbove(e) {
+				return elevio.MD_Up, elevio.EB_Moving
+			}
+			return elevio.MD_Stop, elevio.EB_Idle
+
+		case elevio.MD_Stop:
+			if hasRequestAtFloor(e,b) {
+				return elevio.MD_Stop, elevio.EB_DoorOpen
+			} else if requestsAbove(e) {
+				return elevio.MD_Up, elevio.EB_Moving
+			} else if requestsBelow(e) {
+				return elevio.MD_Down, elevio.EB_Moving
+			}
+			return elevio.MD_Stop, elevio.EB_Idle
+		}
+		//Should never reach this point
+		return elevio.MD_Stop, elevio.EB_Idle
+
+
 
 }
 
-func ReqestShouldClearImmideatly() {
-
+func ReqestShouldClearImmideatly(e elevio.Elevator, floor int, b elevio.ButtonType) bool {
+	if (e.CurrentFloor == floor &&
+	(e.Direction == elevio.MD_Up && b == elevio.BT_HallUp) ||
+		(e.Direction == elevio.MD_Down && b == elevio.BT_HallDown) ||
+		e.Direction == elevio.MD_Stop ||
+		b == elevio.BT_Cab) {
+		return true
+		}
+	return false
 }
 
-func RequestClearAtCurrentFloor() {
+
+
+func RequestClearAtCurrentFloor(e elevio.Elevator) elevio.Elevator {
+	for i, req := range e.Requests {
+		if req.Floor == e.CurrentFloor && req.Button==elevio.BT_Cab {
+			e.Requests = append(e.Requests[:i], e.Requests[i+1:]...)
+		}
+	}
+	return e
+	
 
 }

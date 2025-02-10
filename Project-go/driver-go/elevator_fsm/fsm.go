1fe3c16b532f7422d7cf24b5b6f3eb4601039558
Atle <aabystro@stud.ntnu.no>
2025-02-10 09:40:44 +0100

    ✨ feat(Stuff):

--- Project-go/driver-go/elevator_fsm/fsm.go
+++ Project-go/driver-go/elevator_fsm/fsm.go
@@ -8,14 +8,17 @@ import (
 )
 
 var (
-	 //Define elevator type
-	DRV_buttons = make(chan elevio.ButtonEvent)
-	
+	//Define elevator type
+	e = elevio.Elevator{
+		CurrentFloor: 0,
+		Direction:    elevio.MD_Stop,
+		Behaviour:    elevio.EB_Idle,
+		Requests:     []elevio.ButtonEvent{},
+		NumFloors:    4,
+	}
 )
 
-
-
-func FSM_onFloorArrival(floor int, e *elevio.Elevator) {
+func FSM_onFloorArrival(floor int, drv_button chan elevio.ButtonEvent) {
 
 	elevio.SetFloorIndicator(floor)
 	e.CurrentFloor = floor
@@ -28,7 +31,7 @@ func FSM_onFloorArrival(floor int, e *elevio.Elevator) {
 	elevio.SetMotorDirection(e.Direction)
 
 	if e.Direction == elevio.MD_Stop {
-		e.Requests = doors.OpenDoor( e.CurrentFloor, e.Requests, e.NumFloors, DRV_buttons)
+		e.Requests = doors.OpenDoor(e.CurrentFloor, e.Requests, e.NumFloors, drv_button)
 		e.Requests = elevio.RemoveFromQueue(e.CurrentFloor, e.Direction, e.Requests)
 		elevio.LightButtons(e.Requests, e.NumFloors)
 		if len(e.Requests) != 0 {
@@ -46,7 +49,7 @@ func FSM_onFloorArrival(floor int, e *elevio.Elevator) {
 	// e.Behaviour=elevio.EB_Moving
 
 	// switch e.Behaviour {
-	// case elevio.EB_Moving: 
+	// case elevio.EB_Moving:
 	// 	if(requests.RequestShouldStop(*e)){
 	// 		elevio.SetMotorDirection(elevio.MD_Stop)
 	// 		elevio.SetDoorOpenLamp(true)
@@ -61,12 +64,34 @@ func FSM_onFloorArrival(floor int, e *elevio.Elevator) {
 
 	// }
 
+}
+func init_elevator(drv_floors chan int) {
 
+	for a := 0; a < e.NumFloors; a++ {
+		for i := elevio.ButtonType(0); i < 3; i++ {
+			elevio.SetButtonLamp(i, a, false)
+		}
+	}
+	fmt.Println("After loop")
+
+	e.Direction = elevio.MD_Up
+	fmt.Println(e.Direction)
+
+	elevio.SetMotorDirection(e.Direction)
+
+	elevio.SetDoorOpenLamp(false)
+	e.CurrentFloor = <-drv_floors
+	fmt.Println("After set dir")
+
+	e.CurrentFloor = e.CurrentFloor
+	e.Direction = elevio.MD_Stop
+	elevio.SetMotorDirection(e.Direction)
+	fmt.Println("End of init")
 
 }
 
-func FSM_onButtonPress(e *elevio.Elevator, b elevio.ButtonEvent) {
-	
+func FSM_onButtonPress(b elevio.ButtonEvent) {
+
 	fmt.Printf("%+v\n", b)
 
 	e.Requests = elevio.AddToQueue(b.Button, b.Floor, e.Requests)
@@ -74,4 +99,36 @@ func FSM_onButtonPress(e *elevio.Elevator, b elevio.ButtonEvent) {
 	e.Direction = elevator_motion.SetDirection(e.CurrentFloor, e.Requests[0].Floor, e.Direction)
 	elevio.SetMotorDirection(e.Direction)
 	e.Behaviour = elevio.EB_Moving
+}
+
+func Main_FSM(drv_buttons chan elevio.ButtonEvent, drv_floors chan int, drv_obstr chan bool, drv_stop chan bool) {
+	fmt.Println("here")
+	init_elevator(drv_floors)
+
+	for {
+		select {
+		case a := <-drv_buttons:
+			FSM_onButtonPress(a)
+
+		case a := <-drv_floors:
+
+			FSM_onFloorArrival(a, drv_buttons)
+
+		case a := <-drv_obstr:
+			fmt.Printf("%+v\n", a)
+			if a && doors.IsDoorOpen {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			} else {
+				elevio.SetMotorDirection(e.Direction)
+			}
+
+		case a := <-drv_stop:
+			fmt.Printf("%+v\n", a)
+			for f := 0; f < e.NumFloors; f++ {
+				for b := elevio.ButtonType(0); b < 3; b++ {
+					elevio.SetButtonLamp(b, f, false)
+				}
+			}
+		}
+	}
 }
--- Project-go/driver-go/main.go
+++ Project-go/driver-go/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"Driver-go/elevator_fsm"
-
-	doors "Driver-go/Doors"
+	"fmt"
+	"time"
 
 	"Driver-go/elevio"
-	"fmt"
 )
 
 var d elevio.MotorDirection
@@ -17,72 +16,24 @@ var drv_buttons = make(chan elevio.ButtonEvent)
 var drv_floors = make(chan int)
 var drv_obstr = make(chan bool)
 var drv_stop = make(chan bool)
-var e = elevio.Elevator{
-	CurrentFloor: 0,
-	Direction:    elevio.MD_Stop,
-	Behaviour:    elevio.EB_Idle,
-	Requests:     []elevio.ButtonEvent{},
-	NumFloors:    4,
-}
-
 
 var b []elevio.ButtonEvent
-
-func init_elevator(e elevio.Elevator) {
-
-	for a := 0; a < numFloors; a++ {
-		for i := elevio.ButtonType(0); i < 3; i++ {
-			elevio.SetButtonLamp(i, a, false)
-		}
-	}
-
-	e.Direction = elevio.MD_Up
-	elevio.SetMotorDirection(e.Direction)
-
-	elevio.SetDoorOpenLamp(false)
-	e.CurrentFloor = <-drv_floors
-	e.CurrentFloor=e.CurrentFloor
-	e.Direction = elevio.MD_Stop
-	elevio.SetMotorDirection(e.Direction)
-
-}
 
 func main() {
 
 	elevio.Init("localhost:15657", numFloors)
-	
 
 	go elevio.PollButtons(drv_buttons)
 	go elevio.PollFloorSensor(drv_floors)
 	go elevio.PollObstructionSwitch(drv_obstr)
 	go elevio.PollStopButton(drv_stop)
+	//Networking go routine
 
-	init_elevator(e)
+	go elevator_fsm.Main_FSM(drv_buttons, drv_floors, drv_obstr, drv_stop)
+
 	for {
-		select {
-		case a := <-drv_buttons:
-		elevator_fsm.FSM_onButtonPress(&e,a )
-			
-
-		case a := <-drv_floors:
-			
-			elevator_fsm.FSM_onFloorArrival(a, &e)
-
-		case a := <-drv_obstr:
-			fmt.Printf("%+v\n", a)
-			if a && doors.IsDoorOpen {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-			} else {
-				elevio.SetMotorDirection(e.Direction)
-			}
-
-		case a := <-drv_stop:
-			fmt.Printf("%+v\n", a)
-			for f := 0; f < numFloors; f++ {
-				for b := elevio.ButtonType(0); b < 3; b++ {
-					elevio.SetButtonLamp(b, f, false)
-				}
-			}
-		}
+		fmt.Println("Hello")
+		time.Sleep(3 * time.Second)
 	}
+
 }

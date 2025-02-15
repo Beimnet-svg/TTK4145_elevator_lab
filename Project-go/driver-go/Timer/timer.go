80eed511ad21f863599aa7830175f3698049fc8c
philliab13 <philliab@stud.ntnu.no>
2025-02-15 16:35:29 +0100

    ✨ feat(Timer): Fixed timer
    Fixed the error where Time.now would be 60 seconds so we would end up never closing the door

--- Project-go/driver-go/Timer/timer.go
+++ Project-go/driver-go/Timer/timer.go
@@ -1,33 +1,79 @@
+// package timer
+
+// import (
+// 	"time"
+// )
+
+// var _timerEndTime int
+// var _timerActive bool
+
+// func StartTimer(duration int) {
+// 	_timerEndTime = duration + time.Now().Second()
+// 	_timerActive = true
+
+// }
+
+// func StopTimer() {
+// 	_timerActive = false
+// }
+
+// func TimerTimeOut() bool {
+// 	return _timerActive && (time.Now().Second() >= _timerEndTime)
+
+// }
+
+// func PollTimer(receiver chan bool) {
+// 	ticker := time.NewTicker(100 * time.Millisecond)
+// 	defer ticker.Stop()
+// 	for {
+// 		select {
+// 		case <-ticker.C:
+// 			receiver <- TimerTimeOut()
+// 		}
+// 	}
+// }
+
 package timer
 
 import (
 	"time"
 )
 
-var _timerEndTime int
+var _timer *time.Timer
 var _timerActive bool
 
 func StartTimer(duration int) {
-	_timerEndTime = duration + time.Now().Second()
-	_timerActive = true
+    _timer = time.NewTimer(time.Duration(duration) * time.Second)
+    _timerActive = true
 }
 
 func StopTimer() {
-	_timerActive = false
+    if _timer != nil {
+        _timer.Stop()
+    }
+    _timerActive = false
 }
 
 func TimerTimeOut() bool {
-	return _timerActive && (time.Now().Second() >= _timerEndTime)
-
+    if !_timerActive {
+        return false
+    }
+    select {
+    case <-_timer.C:
+        _timerActive = false
+        return true
+    default:
+        return false
+    }
 }
 
 func PollTimer(receiver chan bool) {
-	ticker := time.NewTicker(100 * time.Millisecond)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			receiver <- TimerTimeOut()
-		}
-	}
+    ticker := time.NewTicker(100 * time.Millisecond)
+    defer ticker.Stop()
+    for {
+        select {
+        case <-ticker.C:
+            receiver <- TimerTimeOut()
+        }
+    }
 }

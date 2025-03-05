8282a2f013b41f2e0d076b3e11355faf9e83f8d1
Lab_PC <Lab_PC@mail.com>
2025-03-05 12:34:32 +0100

    Testing UDP

--- Exercise 2(1)/server.go
+++ Exercise 2(1)/server.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	conn, _ := net.ListenPacket("udp", ":20007")
-	defer conn.Close()
+	localAdress, _ := net.ResolveUDPAddr("udp", ":20007")
+	connection, err := net.ListenUDP("udp", localAdress)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer connection.Close()
 
 	buffer := make([]byte, 1024)
 	fmt.Println("Listening for UDP on port 20007........")
 	for {
-		n, addr, _ := conn.ReadFrom(buffer)
+		n, addr, _ := connection.ReadFrom(buffer)
 
 		fmt.Printf("Received %s from %s\n", string(buffer[0:n]), addr)
 	}
